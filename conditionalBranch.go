81a54a03c3bc0a51ab6f61b92b60bb1003a67cfa
KAZUKI27854 <sadakazuki@team-lab.com>
2021-12-01 10:25:38 +0900

    conditionalBranch.goのリファクタリング

--- conditionalBranch.go
+++ conditionalBranch.go
@@ -5,15 +5,14 @@ import (
 	"math/rand"
 )
 
-
 // 条件分岐まとめ
 func main() {
-	
+
 	// if文による条件分岐1
 	fmt.Println("if文による条件分岐1")
 
 	for i := 0; i < 5; i++ {
-		if i % 2 == 0 {
+		if i%2 == 0 {
 			fmt.Println(i, "は偶数")
 		} else {
 			fmt.Println(i, "は奇数")
@@ -39,27 +38,27 @@ func main() {
 	fmt.Println("switch文による条件分岐")
 
 	for i := 0; i < 5; i++ {
-		
+
 		// どれかのケースに該当した時点で暗黙的にbreak処理が行われる（明示的に記述することも可能）
 		switch i {
 		case 0:
-		       fmt.Println("0")
+			fmt.Println("0")
 		case 1, 2:
-		       fmt.Println("1または2")
+			fmt.Println("1または2")
 		case 3:
-			   fallthrough  // 暗黙的なbreak処理を行わずに、すぐ下のcase節の処理を実行する
+			fallthrough // 暗黙的なbreak処理を行わずに、すぐ下のcase節の処理を実行する
 		default:
-		       fmt.Println("その他")
+			fmt.Println("その他")
 		}
 
 		// switch trueとすることで、case節の式に条件式が使えるようになり、利用範囲が広がる（trueは省略可能）
 		switch true {
 		case i == 0:
-			    fmt.Println("0")
+			fmt.Println("0")
 		case i < 3:
-			    fmt.Println("1または2") 
+			fmt.Println("1または2")
 		default:
-			    fmt.Println("その他")
+			fmt.Println("その他")
 		}
 	}
 }
